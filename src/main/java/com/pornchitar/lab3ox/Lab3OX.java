31a0ac6328c14ba1c59aed39954a4c4703a8a52c
Pornchitar Sarsai <kream21574@gmail.com>
2023-07-19 13:52:40 +0700

    first commit

---
+++ src/main/java/com/pornchitar/lab3ox/Lab3OX.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pornchitar.lab3ox;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Lab3OX {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
